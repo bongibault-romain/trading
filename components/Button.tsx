3687d83ed3f768aa1809fe163bf715b1f1543e37
Bongibault Romain <bongibault.romain@gmail.com>
2025-09-19 14:32:07 +0200

    feat: add Button component

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+import React, { PropsWithChildren  } from 'react'
+
+export function Button(props: PropsWithChildren<React.ButtonHTMLAttributes<HTMLButtonElement>>) {
+  return (
+    <button className="bg-gray-600 px-4 py-2 text-white cursor-pointer hover:bg-gray-500 transition-colors duration-300" {...props}>
+      {props.children}
+    </button>
+  )
+}
+export default Button

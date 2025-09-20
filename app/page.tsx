c59f5aaadb8c3ed2033f56b5c078bc79811403ab
Bongibault Romain <bongibault.romain@gmail.com>
2025-09-20 12:20:42 +0200

    feat: implement socket context management with connection handling and loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,38 @@
+'use client';
+
+import { useSocket } from "@/hooks/useSocket";
 import MdiChevronRight from "../components/icons/MdiChevronRight";
 import Button from "@/components/Button";
 
 export default function Home() {
+  const { connect, isConnected, isStarted } = useSocket();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const formData = new FormData(e.currentTarget);
+    const nickname = formData.get("nickname") as string;
+
+    connect(nickname);
+  };
+
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
-      <form>
-        <div className="flex">
+      <form onSubmit={handleSubmit}>
+        { !isConnected && (<div className="flex">
           <input className="bg-gray-700 outline-0 text-white px-4 py-2" type="text" name="nickname" placeholder="Enter your nickname" />
           
-          <Button>
+          <Button type="submit" disabled={isStarted}>
             <MdiChevronRight width={24} height={24} />
           </Button>
+        </div>) }
+
+        { isConnected && (<div className="flex items-center flex-col gap-8">
+        <span className="loader"></span>
+          <div className="text-white text-lg">Waiting for other players...</div>
+        
         </div>
+        )}
       </form>
     </main>
   );
--- contexts/socketContext.tsx
+++ contexts/socketContext.tsx
@@ -1,14 +1,47 @@
-import React, { createContext, PropsWithChildren, ReactNode } from "react";
-import { Socket } from "socket.io-client";
+import React, { createContext, PropsWithChildren, ReactNode, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
 
 interface SocketContextType {
-  socket: Socket | null;
+  connect: (nickname: string) => void;
+  isConnected: boolean;
+  isStarted: boolean; 
 }
 
 export const SocketContext = createContext<SocketContextType | null>(null);
 
 export const SocketProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [isConnected, setIsConnected] = React.useState(false);
+  const [isStarted, setIsStarted] = React.useState(false);
+  const [socket, setSocket] = React.useState<Socket | null>(null);
+
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_API_URL) {
+      console.error("NEXT_PUBLIC_API_URL is not defined");
+      return;
+    }
+
+    setSocket(io(process.env.NEXT_PUBLIC_API_URL))
+  }, [])
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("joinedRoom", () => {
+      setIsConnected(true);
+      setIsStarted(false);
+    })
+
+  }, [socket])
+
+  const connect = (nickname: string) => {
+    if (!socket) return;
+
+    socket.emit("joinGame", { nickname });
+  }
+
   return (
-    <SocketContext.Provider value={{ socket: null }}></SocketContext.Provider>
+    <SocketContext.Provider value={{ connect, isConnected, isStarted }}>
+      {children}
+    </SocketContext.Provider>
   );
 };
